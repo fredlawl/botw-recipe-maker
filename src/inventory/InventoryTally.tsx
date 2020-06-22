3becf204084a360ddae22688919fb88964be2e4a
Frederick Lawler <fred@fredlawl.com>
2020-06-21 19:35:35 -0500

    InventoryTally: Sort by stack DESC
    InventoryTally: Sort by stack DESC.

--- src/inventory/InventoryTally.tsx
+++ src/inventory/InventoryTally.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import Inventory from "./Inventory";
+import IngredientBundle from "../ingredient/IngredientBundle";
 
 interface InventoryTallyProps {
 	inventory: Inventory
 }
 
 const InventoryTally = (props: InventoryTallyProps) => {
+	const sortFunc = (a: IngredientBundle, b: IngredientBundle) => {
+		if (a.amount === b.amount) return 0;
+		return (a.amount > b.amount) ? -1 : 1;
+	};
+
 	return (
 		<section className={"inventory-tally"}>
 			<header>
-				<h1>Total Items Selected: {props.inventory.totalCount}</h1>
+				<h1>Ingredients Selected: {props.inventory.totalCount}</h1>
 			</header>
 			{props.inventory.totalCount > 0 &&
 				<ul>
-					{props.inventory.items.map(i =>
+					{props.inventory.items.sort(sortFunc).map(i =>
 						<li key={i.ingredient.id}>{i.ingredient.name}: {i.amount}</li>)}
 				</ul>
 			}
